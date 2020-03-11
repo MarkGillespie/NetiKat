7aad3be1522916c033f6b1156fcfc8f6cba2eaed
Mark Gillespie <mark.gillespie81@gmail.com>
2020-03-11 13:13:20 -0400

    Sample files

---
+++ src/tests/SampleTest.h
@@ -0,0 +1,6 @@
+class SampleTest : public ::testing::Test {};
+
+// ============================================
+//        Sample Test
+// ============================================
+TEST_F(SampleTest, KleinToPoincare) { EXPECT_TRUE(true); }
---
+++ src/tests/test.cpp
@@ -0,0 +1,12 @@
+#include "test_utils.h"
+
+#include <gtest/gtest.h>
+
+// clang-format off
+#include "SampleTest.h"
+// clang-format on
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
